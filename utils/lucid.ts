1ecffbb261e68fd1a509b6febe26ac8c714651e3
rodrigomd94 <51804048+rodrigomd94@users.noreply.github.com>
2022-10-10 13:43:07 -0600

    Change blockfrost url

--- utils/lucid.ts
+++ utils/lucid.ts
@@ -5,8 +5,8 @@ const initLucid = async (wallet: string) => {
         wallet.toLowerCase()
     ].enable())
     const lucid = await Lucid.new(
-        new Blockfrost('https://cardano-preprod.blockfrost.io/api/v0', process.env.NEXT_PUBLIC_BLOCKFROST as string),
-        'Preprod')
+        new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', process.env.NEXT_PUBLIC_BLOCKFROST as string),
+        'Mainnet')
     lucid.selectWallet(api)
     //setLucid(lucid)
     return lucid;
